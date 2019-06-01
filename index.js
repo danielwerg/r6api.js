645cf7da5f440353769b5c2fcd854eac18220338
Lars Brix <git@laxis.me>
2019-06-01 12:14:17 +0200

    Update index.js
    This imports the methods once and assigns them later, instead of calling require every instanciation.
    It will help the (inevitable) move to es6 modules later, and is cleaner in general

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const auth = require('./lib/auth.js');
+const getStatus = require('./lib/methods/getStatus.js');
+const getById = require('./lib/methods/getById.js');
+const getByUsername = require('./lib/methods/getByUsername.js');
+const getLevel = require('./lib/methods/getLevel.js');
+const getPlaytime = require('./lib/methods/getPlaytime.js');
+const getRank = require('./lib/methods/getRank.js');
+const getStats = require('./lib/methods/getStats.js');
+const custom = require('./lib/methods/custom.js');
+const constants = require('./lib/constants');
+const errors = require('./lib/errors.js');
 
 class R6API {
   constructor(email, password) {
     auth.setCredentials(email, password);
     this.auth = auth;
-    this.getStatus = require('./lib/methods/getStatus.js');
-    this.getId = require('./lib/methods/getById.js');
-    this.getUsername = require('./lib/methods/getByUsername.js');
-    this.getLevel = require('./lib/methods/getLevel.js');
-    this.getPlaytime = require('./lib/methods/getPlaytime.js');
-    this.getRank = require('./lib/methods/getRank.js');
-    this.getStats = require('./lib/methods/getStats.js');
-    this.custom = require('./lib/methods/custom.js');
-    this.constants = require('./lib/constants');
-    this.errors = require('./lib/errors.js');
+    this.getStatus = getStatus,
+    this.getId = getById,
+    this.getUsername = getByUsername,
+    this.getLevel = getLevel,
+    this.getPlaytime = getPlaytime,
+    this.getRank = getRank,
+    this.getStats = getStats,
+    this.custom = custom,
+    this.constants = constants,
+    this.errors = errors,
   }
 }
 
