3740a634d27b799978dee44d958f73344a4c65f4
Federico Grandi <fgrandi30@gmail.com>
2019-11-30 16:36:21 +0100

    I love async error handling

--- scripts/response.js
+++ scripts/response.js
@@ -2,20 +2,20 @@ const R6API = require('../index');
 const fs = require('fs');
 const path = require('path');
 
-try {
-  const email = process.env.API_EMAIL,
-    pass = process.env.API_PASS;
-  if (!email || !pass) throw new Error("Cannot update file without API credentials");
+const email = process.env.API_EMAIL,
+  pass = process.env.API_PASS;
+if (!email || !pass) throw new Error("Cannot update file without API credentials");
 
-  const r6 = new R6API(email, pass);
+const r6 = new R6API(email, pass);
 
-  (async () => {
+(async () => {
+  try {
     const username = 'Daniel.Nt';
     const id = await r6.getId('uplay', username).then(el => el[0].id);
     const stats = await r6.getStats('uplay', id).then(el => el[0]);
 
     fs.writeFileSync(path.join(__dirname, '../doc/stats-response.json'), JSON.stringify(stats));
-  })();
-} catch (e) {
-  throw e;
-}
+  } catch (e) {
+    throw e;
+  }
+})();
