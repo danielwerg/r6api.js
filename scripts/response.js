db751a05fc0f7d8c288a48eb94679c991ac68b6e
Federico Grandi <fgrandi30@gmail.com>
2019-11-30 16:32:00 +0100

    Avoid pending status

--- scripts/response.js
+++ scripts/response.js
@@ -2,16 +2,20 @@ const R6API = require('../index');
 const fs = require('fs');
 const path = require('path');
 
-const email = process.env.API_EMAIL,
-  pass = process.env.API_PASS;
-if (!email || !pass) throw new Error("Cannot update file without API credentials");
+try {
+  const email = process.env.API_EMAIL,
+    pass = process.env.API_PASS;
+  if (!email || !pass) throw new Error("Cannot update file without API credentials");
 
-const r6 = new R6API(email, pass);
+  const r6 = new R6API(email, pass);
 
-(async () => {
-  const username = 'Daniel.Nt';
-  const id = await r6.getId('uplay', username).then(el => el[0].id);
-  const stats = await r6.getStats('uplay', id).then(el => el[0]);
+  (async () => {
+    const username = 'Daniel.Nt';
+    const id = await r6.getId('uplay', username).then(el => el[0].id);
+    const stats = await r6.getStats('uplay', id).then(el => el[0]);
 
-  fs.writeFileSync(path.join(__dirname, '../doc/stats-response.json'), JSON.stringify(stats));
-})();
+    fs.writeFileSync(path.join(__dirname, '../doc/stats-response.json'), JSON.stringify(stats));
+  })();
+} catch (e) {
+  throw e;
+}
