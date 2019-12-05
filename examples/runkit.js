1459e12cb31209586a158c3e99843d14d603c48e
Daniel <35052399+danielwerg@users.noreply.github.com>
2019-12-06 02:50:59 +0300

    Update runkit example

--- examples/runkit.js
+++ examples/runkit.js
@@ -1,8 +1,10 @@
 const R6API = require('r6api.js');
-const r6 = new R6API('example@mail.com', 'eatbigbanan');
+const r6api = new R6API('example@mail.com', 'eatbigbanan');
 
-const username = 'Daniel.Nt';
-const id = await r6.getId('uplay', username).then(el => el[0].id);
-const stats = await r6.getStats('uplay', id).then(el => el[0]);
+const username = 'Daniel.Nt',
+      platform = 'uplay';
 
-console.log(`${username} has played ${stats.pvp.general.matches} matches`);
+const id = await r6api.getId(platform, username).then(el => el[0].id);
+const stats = await r6api.getStats(platform, id).then(el => el[0]);
+
+console.log(`${username} has played ${stats.pvp.general.matches} matches.`);
