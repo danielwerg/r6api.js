1833caa3869326a1702d251024a38bd8ef144ceb
Daniel <35052399+danielwerg@users.noreply.github.com>
2020-04-10 19:16:32 +0300

    Fix typo

--- scripts/response.js
+++ scripts/response.js
@@ -6,7 +6,7 @@ const filePath = join(__dirname, '../doc/getStats-response.json');
 const email = process.env.UBI_EMAIL,
       password = process.env.UBI_PASSWORD;
 let secretsError = ''
-if (!email) scecretsError += `Invalid email secret: ${email === '' ? 'empty string' : typeof email}\n`
+if (!email) secretsError += `Invalid email secret: ${email === '' ? 'empty string' : typeof email}\n`
 if (!password) secretsError += `Invalid password secret: ${password === '' ? 'empty string' : typeof password}\n`
 if (secretsError) throw new Error(secretsError.trim())
 
