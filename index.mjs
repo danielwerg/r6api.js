f2de2df976bbd3a32430f3b34504abe681c79c83
BadCoder1337 <anton.tiz.musin@gmail.com>
2021-08-28 18:46:05 +0300

    Re-export module as MJS

---
+++ index.mjs
@@ -0,0 +1,6 @@
+import R6APIModule from './dist/index.js';
+
+const { constants, fetch, typings, utils, default: R6API } = R6APIModule;
+
+export default R6API;
+export { constants, fetch, typings, utils };
